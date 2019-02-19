ab1973fb9272b4e7a9ba4f6ac8f3408b6f85c5f4
zhoukailun <35447302+zhoukailun@users.noreply.github.com>
2019-02-19 11:10:40 +0800

    Update default.py

--- conf/default.py
+++ conf/default.py
@@ -31,9 +31,9 @@ from django.conf.global_settings import *  # noqa
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -> 点击应用ID -> 基本信息 中获取 APP_ID 和 APP_TOKEN 的值
 APP_ID = 'bnsapp'
-APP_TOKEN = 'e04c47ad-4929-4977-b33b-ddc9728ab414'
+APP_TOKEN = '122127bd-b1ee-447a-a7ce-29bf9e927b01'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
-BK_PAAS_HOST = 'http://paas.bk.test2.garena.com:81'
+BK_PAAS_HOST = 'http://paas.cptbtptp.blueking.com'
 
 
 # 是否启用celery任务
